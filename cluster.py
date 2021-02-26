113fd9b465216b4336a985bf2040e046f097dab1
tiffanyhltse <32942150+tiffanyhltse@users.noreply.github.com>
2021-02-25 18:54:03 -0800

    Add files via upload

---
+++ cluster.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 17 20:03:08 2021
+
+@author: tiffanytse
+"""
+
+class cluster(object):
+
+    def __init__(self):
+        pass
+
+    def fit(self, X):
+        pass
+    
+    
+    
+    
